c30be1b40a4bc6d7d2cba6d8f07638c385ce21ef
Juliano Garcia <julianogarcia_1997@hotmail.com>
2016-10-08 13:45:31 -0300

    Finish the infinite monkey theorem with optimization

--- smpl2.py
+++ smpl2.py
@@ -5,33 +5,49 @@ import random as rnd
 --> The infinite monkey theorem <--
 - sentence: "methinks it is like a weasel"
 '''
-# Return a random string
-def rndString(n):
-    return "".join([rnd.choice(string.ascii_lowercase+" ") for i in range(n)])
 
-def score(strGen, target):
-    score = [strGen[i]==target[i] for i in range(min(len(strGen), len(target)))]
-    return sum(score)
 
-def checker(target, n):
+# Return a random string, provided a placeholder
+# The placeholder tells which position of the generated string will me random
+def rndString(n, plc, target):
+    xStr = list()
+    for i in range(n):
+        if not plc[i]:
+            xStr.append(rnd.choice(string.ascii_lowercase+" "))
+        else:
+            xStr.append(target[i])
+    return "".join(xStr)
+
+
+def score(strGen, target, plc, n):
+    print(strGen)
+    score = [strGen[i] == target[i] for i in range(n)]
+    if sum(score) > sum(plc):
+        plc = score
+    return sum(plc), plc
+
+
+def checker(target, n, plc):
     maxN = counter = 0
     bstStr = ""
     while maxN != n:
-        strGen = rndString(n)
-        nGen = score(strGen, target)
+        strGen = rndString(n, plc, target)
+        nGen, plc = score(strGen, target, plc, n)
         if nGen > maxN:
             maxN = nGen
             bstStr = strGen
         counter += 1
-        if counter%1000 == 0:
-            print("Best String: %s , Score = %d" %(bstStr, maxN))
-    print("Success! String found in %d tries!" %(counter - 1))
+        if counter % 1000 == 0:
+            print("Best String: %s , Score = %d" % (bstStr, maxN))
+            print("PLC = %s " % (plc))
+    print("Success! String found in %d tries!" % (counter - 1))
 
 
 def main():
     targetString = "methinks it is like a weasel"
     n = len(targetString)
-    checker(targetString, n)
+    plc = [0]*n
+    checker(targetString, n, plc)
 
 if __name__ == '__main__':
     main()
