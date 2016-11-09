99a2d59e44725d16dfb20eae621be4c1cf6dd117
Juliano - Robotenique <julianogarcia_1997@hotmail.com>
2016-11-09 13:41:23 -0200

    Update smpl4 with functions to sum using np and via python standart functions

--- smpl4.py
+++ smpl4.py
@@ -1,21 +1,36 @@
 '''
 2.1 - Numpy Array library
 '''
+#!/usr/bin/env
 import numpy as np
 
 def main():
-    n = 10
-    a = [x**2 for x in range(n)]
-    b = [x**3 for x in range(n)]
+    ''''
+    # 1) Create two vectors 'a' and 'b', and sum them.
+    # Standart
+    n = 3
+    a = [x**2 for x in range(1, n + 1)]
+    b = [x**3 for x in range(1, n + 1)]
     v = plainVectorAddition(a, b)
+    # Numpy
+    n = 3
+    a = np.arange(1, n + 1) ** 2
+    b = np.arange(1, n + 1) ** 3
+    v = npSum(a, b)
+    '''
     print(v)
 
 
-def plainVectorAddition(a, b):
-    v = list()
-    for i,j,k in zip(a, b, len(a)):
-        v[k] = i + j
-    return v
+def plainSum(a, b):
+    '''
+    Sum two vectors using standart python functions
+    '''
+    return [i + j for i,j in zip(a,b)]
+
+def npSum(a, b):
+    return a + b
+
+
 
 if __name__ == '__main__':
     main()
