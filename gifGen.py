4d0ada247a4a86e15596ae6cc27dbba92e0a8795
Juliano - Robotenique <julianogarcia_1997@hotmail.com>
2016-11-08 22:30:50 -0200

    Update comment descriptions and remove demo images

--- gifGen.py
+++ gifGen.py
@@ -3,13 +3,27 @@ import matplotlib.image as img
 import matplotlib.pyplot as plt
 import numpy as np
 
+'''
+    Rotates the color vector of the image into the 3D space,
+    by the angle 'theta' provided, then generates a gif image.
+'''
+
 def n(im):
+    '''
+    Normalize the image
+    '''
     return -np.log(1/((1 + im)/257) - 1)
 
 def uN(im):
+    '''
+    Un-Normalize the image
+    '''
     return (1 + 1/(np.exp(-im) + 1) * 257).astype("uint8")
 
 def rM(t):
+    '''
+    Rotation Matrix in 3D by the angle 't'
+    '''
     return np.c_[
     [1,0,0],
     [0,np.cos(t), -np.sin(t)],
@@ -18,6 +32,7 @@ def rM(t):
 
 def upd(i):
     iN = n(im)
+    # Do matrix multiplication
     iR = np.einsum("ijk, lk->ijl", iN, rM(i * np.pi/10))
     im2 = uN(iR)
     ax.imshow(im2)
--- imgProcessing.py
+++ imgProcessing.py
@@ -26,22 +26,22 @@ def rotationMatrix(theta):
 		[0, np.sin(theta), np.cos(theta)]]
 
 def plotImage(im, h = 8, **kwargs):
-	# Slice Image : im = im[100:300,:200,:] 
+	# Slice Image : im = im[100:300,:200,:]
 	y = im.shape[0]
 	x = im.shape[1]
-	w = (y/x) * h	
+	w = (y/x) * h
 	plt.figure(figsize = (w,h))
 	plt.imshow(im, interpolation = "none", **kwargs)
 	plt.axis('off')
 	plt.show()
-	'''	
+	'''
 	fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(15,5))
 	for c, ax in zip(range(3), axs):
 		# Convert data types to INT
 		tmp = np.zeros(im.shape, dtype="uint8")
 		tmp[:, : ,c] = im[: , :, c]
 		ax.imshow(tmp)
-		ax.set_axis_off()		
+		ax.set_axis_off()
 	plt.show()
 	'''
 
