b51b88d6b3b81d757a616b50f113c271b8f6bb1c
Juliano - Robotenique <julianogarcia_1997@hotmail.com>
2016-12-02 17:09:57 -0200

    Add distributions python script

---
+++ statistics/distributions.py
@@ -0,0 +1,19 @@
+'''
+Probability distributions - Implementation of the calculation of
+the most used discrete probability distribution in statistics!
+'''
+import numpy as np
+import matplotlib as plt
+
+
+def main():
+    pass
+
+def binomial(n, p):
+    
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
