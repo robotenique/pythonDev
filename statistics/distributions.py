b24bfd8a28b551d293c0991628bee3aff97988fa
Juliano Garcia <julianogarcia_1997@hotmail.com>
2016-12-03 22:37:48 -0200

    Add the poisson distribution

--- statistics/distributions.py
+++ statistics/distributions.py
@@ -5,30 +5,37 @@ probability distribution in statistics!
 '''
 import numpy as np
 import matplotlib as plt
+import math as m
 from fractions import Fraction
 from functools import reduce
 
-
 def main():
-    lenProb = 2
+    lenProb = 3
     menu = [[] for x in range(lenProb)]
     menu[0] = [
         "--- Welcome to the ProbDistributions =) ---\n",
         "You may choose one of the options below: \n",
-        "[1] - Binomial Distribution\n"
+        "[1] - Binomial Distribution\n",
+        "[2] - Poisson Distribution\n"
     ]
     menu[1] = [
-        "\n---BINOMIAL DISTRIBUTION: k sucesses in n tries, with probability p ---",
+        "\n---BINOMIAL DISTRIBUTION: k sucesses in n tries, with probability p (each try) ---",
         "n",
         "p",
         "k"
     ]
+    menu[2] = [
+        "\n---POISSON DISTRIBUTION: k sucesses in a given time with rate λ ---",
+        "k",
+        "λ",
+    ]
     readIn = lambda x: float(input(x + " = "))
-    probDistr = [0, binomial]
+    probDistr = [0, binomial, poisson]
     try:
         print("".join(menu[0]))
         io = int(input("==> "))
         if io < 1 or io >= lenProb: raise ValueError()
+        print(menu[io][0])
         args = list(map(readIn, (menu[io][i] for i in range(1, len(menu[io])))))
         prob = probDistr[io](args)
         print("The " + probDistr[io].__name__ + " probability is " + str(round(prob, 3)) + "!")
@@ -54,12 +61,21 @@ def binomial(*args):
         n, p, k = int(args[0][0]), args[0][1], int(args[0][2])
         if p < 0 or p > 1:
             raise ValueError("Wrong probability value ("+str(p)+") !")
-
-        # FIXME
-        return(binCoeff(n, k)*(p**k)*(1- p)**(n - k))
+        return(binCoeff(n, k)*(p**k)*((1- p)**(n - k)))
     except ValueError as err:
         print(";( Error: ", err)
         exit()
+
+def poisson(*args):
+    try:
+        k, rate = int(args[0][0]), args[0][1]
+        if k > 20000:
+            raise ValueError("Whoa, go do this somewhere else! >:(  !")
+        return((m.exp(-rate)*(rate**k))/m.factorial(k))
+    except ValueError as err:
+        print(err)
+        exit()
+
 
 
 if __name__ == '__main__':
