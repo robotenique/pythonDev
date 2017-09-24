08a7819309425e3b2184159676a6e85320b004f9
robotenique <julianogarcia_1997@hotmail.com>
2017-09-24 17:30:36 -0300

    add new dp problem set

---
+++ dynamicProg/cut_rod.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def main():
+    NUM_RODS = 10
+    MAX_PRICE = 20
+    # The price array
+    p = np.random.rand(NUM_RODS + 1) * MAX_PRICE
+
+
+if __name__ == '__main__':
+    main()
